fbab12eb52a45d7a180ebe0fec49f95bc40f1eb0
Pourab Karchaudhuri <36023@Hexaware.com>
2019-02-03 18:51:24 +0530

    Updated with stable code

--- index.js
+++ index.js
@@ -3,42 +3,42 @@ var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-var fs = require('fs');
-var path = require('path')
-var request = require('request');
+
+var scribe = require('scribe-js')(),
+    app    = express();
+
+app.use('/logs', scribe.webPanel());
 
 require('dotenv').config()
-//Load Module dependencies
 
 var ai = require('./services/conversation')
+var intent = require('./src/intent');
 
-//Configuring the Express Middleware
-var app = express();
-
-var accessLogStream = fs.createWriteStream(__dirname + '/logs/access.log', {flags: 'a'})
-
-//Configure Morgan's Logging Formats
-// app.use(morgan('common', {stream: accessLogStream}))    //UNCOMMENT TO ENABLE FILE LOGGING
 app.use(morgan('common'));
+app.use(scribe.express.logger());
 
-
-//Set PORT to Dynamic Environments to run on any Server
 var port = process.env.PORT || 5000;
 
-//Configure Express to Recieve JSON and extended URLencoded formats
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
 
 app.use(bodyParser.json({
     limit: "50mb"
 }))
-//Set RESTful routes file size limit
+
+var console = process.console;
 
 app.get('/', function(req, res) {
   res.send("Server Online");
 });
 //Route for GET
+
+
+// With log(...)
+// console.log("Hello World!");
+// console.info("Hello World!");
+// console.error("Hello World!");
+// console.warning("Hello World!");
 
 app.post('/v1/api/query', function(req, res) {
     console.log("Incoming payload!")
@@ -48,15 +48,17 @@ app.post('/v1/api/query', function(req, res) {
       if(err){
         console.log("Respond with Server Unavailable");
       }
-
       else{
-        console.log("Respond with success");
-        res.send(result)
+        intent.IntentParser(result, (err, result) => {
+          res.json(result);
+        })
       }
     })
-    // var token = req.body.token;
-    // var geo = req.body.geo;
-    // ai.ConversationHandler((req.))
+});
+
+app.post('/v1/api/channel', function(req, res) {
+  console.log("Incoming Channel!")
+  console.log(JSON.stringify(req.body))
 });
 
 app.listen(port);
---
+++ src/intent.js
@@ -0,0 +1,123 @@
+
+module.exports = {
+    'IntentParser': function(payload, callback){
+        console.log('Intent Response : ' + JSON.stringify(payload));
+        console.log('Intent Name : ', payload.result.action);
+
+        var action = payload.result.action;
+        var actionIncomplete = payload.result.actionIncomplete;
+        var parameters = payload.result.parameters;
+        
+        if(action == "IncidentRequest"){
+            console.log("Incident Request Intent Triggered!");
+            if(!actionIncomplete){
+                console.log("Params fulfilled");
+                //Call ML Model with REST and Get its response
+                //Call Service Now with above response params
+                //Pass on required formatted response from Service Now below::
+
+                callback(null, ResponseBuilderCard(parameters))
+            }
+            else{
+                console.log("Asking Prompts");
+                callback(null, payload);
+            }
+        }
+        else{
+            console.log("Bypass Uninteractive Intents");
+            callback(null, payload);
+        }
+        
+    }
+}
+
+function ResponseBuilderText(data){
+    console.log("Framing Text with Data : " + JSON.stringify(data))
+    var text = { "result" : {
+                    "fulfillment":{
+                    "speech": "",
+                    "displayText": "",
+                    "messages": [{
+                        "type": 1,
+                        "platform": "facebook",
+                        "title": "HOME BROADBAND",
+                        "subtitle": "If you're running multiple devices, part of a big family or streaming on-demand video, then Broadband & nbn™ is just the ticket for your entertainment needs!<br/><br/><br/><br/>",
+                        "imageUrl": "https://smb.optus.com.au/opfiles/Shop/Consumer/Assets/Images/Broadband/broadband-NBN-landing-page-3UP.png",
+                        "buttons": [{
+                            "text": "Show More",
+                            "postback": "https://www.optus.com.au/shop/broadband/home-broadband"
+                            },
+                            {
+                            "text": "Select this plan",
+                            "postback": "HOME BROADBAND"
+                            }
+                        ]
+                    }
+                ]
+            }        
+        }
+    }
+
+    return text;
+}
+
+function ResponseBuilderCard(data){
+    console.log("Framing Card with Data : " + JSON.stringify(data))
+    var card = { "result" : {
+                    "fulfillment":{
+                    "speech": "",
+                    "displayText": "",
+                    "messages": [{
+                        "type": 1,
+                        "platform": "facebook",
+                        "title": "HOME BROADBAND",
+                        "subtitle": "If you're running multiple devices, part of a big family or streaming on-demand video, then Broadband & nbn™ is just the ticket for your entertainment needs!<br/><br/><br/><br/>",
+                        "imageUrl": "https://smb.optus.com.au/opfiles/Shop/Consumer/Assets/Images/Broadband/broadband-NBN-landing-page-3UP.png",
+                        "buttons": [{
+                            "text": "Show More",
+                            "postback": "https://www.optus.com.au/shop/broadband/home-broadband"
+                            },
+                            {
+                            "text": "Select this plan",
+                            "postback": "HOME BROADBAND"
+                            }
+                        ]
+                    }
+                ]
+            }        
+        }
+    }
+
+    return card;
+}
+
+function ResponseBuilderQuickReplies(data){
+    console.log("Framing Quick Replies with Data : " + JSON.stringify(data));
+        var quickReply ={ "result" : {
+            "fulfillment":{
+                "speech": "",
+                "displayText": "",
+                "messages": [{
+                    "type": 4,
+                    "platform": "facebook",
+                    "payload": {
+                        "facebook": {
+                            "text": `Do you want to buy `,
+                            "quick_replies": [{
+                                    "content_type": "text",
+                                    "title": "Yes",
+                                    "payload": "Yes"
+                                }, {
+                                    "content_type": "text",
+                                    "title": "No",
+                                    "payload": "No"
+                                }]
+                        }
+                    }
+                }]
+            }
+        }
+    };
+
+    return quickReply;
+}
