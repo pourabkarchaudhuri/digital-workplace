bb56f7fb693cb4629e61ce76014176f7d6512d32
Pourab Karchaudhuri <36023@Hexaware.com>
2019-02-03 21:41:26 +0530

    Update

--- src/action.js
+++ src/action.js
@@ -64,6 +64,25 @@ function ResponseBuilderGoogleAssistantCard(data){
                   "simpleResponse": {
                     "textToSpeech": "this is a simple response"
                   }
+                },
+                {
+                    "basicCard": {
+                        "title": "Math & prime numbers",
+                        "formattedText": "42 is an even composite number. It\n    is composed of three distinct prime numbers multiplied together. It\n    has a total of eight divisors. 42 is an abundant number, because the\n    sum of its proper divisors 54 is greater than itself. To count from\n    1 to 42 would take you about twenty-one…",
+                        "image": {
+                            "url": "https://example.google.com/42.png",
+                            "accessibilityText": "Image alternate text"
+                        },
+                        "buttons": [
+                            {
+                                "title": "Read more",
+                                "openUrlAction": {
+                                    "url": "https://example.google.com/mathandprimes"
+                                }
+                            }
+                        ],
+                        "imageDisplayOptions": "CROPPED"
+                    }
                 }
               ]
             }
