5ab289e919473f37d9924695ad49a3463acd8768
Pourab Karchaudhuri <36023@Hexaware.com>
2019-02-08 00:57:14 +0530

    Added Jaro

--- src/action.js
+++ src/action.js
@@ -114,7 +114,7 @@ module.exports = {
                     number = number.replace(/ /g, "");
                 }
                 else if(source == "API"){
-                    console.log("Query : ", event.query);
+                    console.log("Query : ", event);
                     number = event.query.replace(/[^0-9]/g, "");
                     number = number.replace(/ /g, "");
                 }
